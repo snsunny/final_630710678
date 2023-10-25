078b6b8eeb03db4b7d18fad5420c00657349244b
snsunny <sujintra444@gmail.com>
2023-10-26 01:04:49 +0700

    Initial commit

---
+++ bin/_final_630710678.dart
@@ -0,0 +1,5 @@
+import 'package:_final_630710678/_final_630710678.dart' as _final_630710678;
+
+void main(List<String> arguments) {
+  print('Hello world: ${_final_630710678.calculate()}!');
+}
---
+++ test/_final_630710678_test.dart
@@ -0,0 +1,8 @@
+import 'package:_final_630710678/_final_630710678.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
